a22bef9636cba3b7fadd35055aac1837193b261b
olivier <olivier.winter@hotmail.fr>
2022-01-11 11:16:33 +0000

    loading functions BWM

--- sources/ephys_atlas/data.py
+++ sources/ephys_atlas/data.py
@@ -1,11 +1,19 @@
-def bwm_pids(one):
+import logging
+
+_logger = logging.getLogger('ibllib')
+
+SPIKES_ATTRIBUTES = ['clusters', 'times', 'depths', 'amps']
+CLUSTERS_ATTRIBUTES = ['channels', 'depths', 'metrics']
+
+
+def bwm_pids(one, tracing=True):
     django_strg = ['session__project__name__icontains,ibl_neuropixel_brainwide_01',
                    'session__qc__lt,50',
                    '~json__qc,CRITICAL',
-                   #'json__extended_qc__tracing_exists,True',  # TODO remove ?
                    'session__extended_qc__behavior,1,'
                    'session__json__IS_MOCK,False']
+    if tracing:
+        django_strg.append('json__extended_qc__tracing_exists,True')
 
     insertions = one.alyx.rest('insertions', 'list', django=django_strg)
     return [item['id'] for item in insertions], insertions
-
