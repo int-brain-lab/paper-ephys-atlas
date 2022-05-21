5687aaec7335c736a50b1b20b5a8a58caef8fc80
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2022-05-21 16:54:36 +0100

    test merge

--- sources/examples/example_03_display_voltage_features.py
+++ sources/examples/example_03_display_voltage_features.py
@@ -79,8 +79,13 @@ ax.legend()
 
     # sum detected spikes
 
+<<<<<<< Updated upstream
 gb_regions = df_voltage.groupby('atlas_id_beryl')
 gb_regions = df_voltage.groupby('atlas_id')
+=======
+gb_regions = df_voltage.groupby('atlas_id')
+
+>>>>>>> Stashed changes
 
 df_regions = gb_regions.agg({
     'rms_ap': ('median', 'var'),
@@ -90,6 +95,7 @@ df_regions = gb_regions.agg({
     'x': 'count',
 })
 
+<<<<<<< Updated upstream
 # # Plot PSD on coronal slice
 # # From https://int-brain-lab.github.io/iblenv/notebooks_external/atlas_plotting_scalar_on_slice.html
 # from ibllib.atlas.plots import plot_scalar_on_slice
@@ -121,3 +127,45 @@ df_regions = gb_regions.agg({
 # ax[1].imshow(ba._label2rgb(ba.regions.mappings['Beryl'][ba.label.flat[image_map]]), origin='upper')
 # ax[2].imshow(ba._label2rgb(ba.regions.mappings['Cosmos'][ba.label.flat[image_map]]), origin='upper')
 # ax[2].imshow(picture, origin='upper')
+=======
+# Plot PSD on coronal slice
+# From https://int-brain-lab.github.io/iblenv/notebooks_external/atlas_plotting_scalar_on_slice.html
+from ibllib.atlas.plots import plot_scalar_on_slice
+
+acro_plot = df_regions['acronym'].values.flatten()[1:]
+data_plot = df_regions['psd_gamma'].values.flatten()[1:]
+# data_plot = df_regions['x'].values.flatten()[1:]
+
+fig, ax = plot_scalar_on_slice(acro_plot, data_plot, coord=-2000, slice='coronal', mapping='Allen', hemisphere='both',
+                               background='boundary', cmap='magma', brain_atlas=ba)
+plt.colorbar(ax.get_images()[0])
+fig.savefig(fname='/Users/gaelle/Documents/Work/SCGB2022/rms_ap_slice_RS.pdf')
+
+fig, ax = plot_scalar_on_slice(acro_plot, data_plot, coord=-5400, slice='coronal', mapping='Allen', hemisphere='both',
+                               background='boundary', cmap='magma', brain_atlas=ba)
+plt.colorbar(ax.get_images()[0])
+fig.savefig(fname='/Users/gaelle/Documents/Work/SCGB2022/rms_ap_slice_cer.pdf')
+
+
+
+
+from iblutil.numerical import ismember
+
+_, ich = ismember(df_regions.index, ba.regions.id[ba.regions.mappings['Beryl']])
+image_map_beryl = ba.regions.mappings['Beryl'][ba.label.flat[image_map]]  # this indexing maps directly
+exists_in_map, ind_image_map_channels = ismember(image_map_beryl, ich)
+
+picture = np.zeros_like(image_map)
+picture[exists_in_map] = df_regions['x'].to_numpy().flatten()[ind_image_map_channels]
+
+
+ba.regions.id[ba.regions.mappings['Beryl'][ba.label.flat[image_map]]]
+
+
+
+fig, ax = plt.subplots(4, 1, figsize=(16, 5))
+ax[0].imshow(ba._label2rgb(ba.label.flat[image_map]), origin='upper')
+ax[1].imshow(ba._label2rgb(ba.regions.mappings['Beryl'][ba.label.flat[image_map]]), origin='upper')
+ax[2].imshow(ba._label2rgb(ba.regions.mappings['Cosmos'][ba.label.flat[image_map]]), origin='upper')
+ax[2].imshow(picture, origin='upper')
+>>>>>>> Stashed changes
