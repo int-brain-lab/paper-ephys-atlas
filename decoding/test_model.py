93032c773b1b537d4d51857619ff3237a2fb9e9c
Guido Meijer <gtmeijer@gmail.com>
2022-05-22 11:56:39 +0100

    UPdate

--- decoding/test_model.py
+++ decoding/test_model.py
@@ -20,10 +20,13 @@ parser = argparse.ArgumentParser()
 # Settings
 parser.add_argument("-data_path", "--data_path", help="Path to training data")
 parser.add_argument("-classifier", "--classifier", help="forest or bayes")
+parser.add_argument("-max_depth", "--max_depth", help="Max depth")
+parser.add_argument("-n_trees", "--n_trees", help="")
+parser.add_argument("-max_leaf_nodes", "--max_leaf_nodes", help="")
 args = parser.parse_args()
 classifier = args.classifier
 CLASSIFIER = 'forest'  # bayes or forest
-N_FOLDS = 5
+N_FOLDS = 10
 FEATURES = ['psd_delta', 'psd_theta', 'psd_alpha', 'psd_beta', 'psd_gamma', 'rms_ap', 'rms_lf',
             'spike_rate']
 
@@ -34,7 +37,8 @@ feature_arr = chan_volt[FEATURES].to_numpy()
 
 # Initialize
 if args.classifier == 'forest':
-    clf = RandomForestClassifier(random_state=42, n_estimators=100, max_depth=50)
+    clf = RandomForestClassifier(random_state=42, n_estimators=args.n_trees, max_depth=args.max_depth,
+                                 max_leaf_node=args.max_leaf_node)
 elif args.classifier == 'bayes':
     clf = GaussianNB()
 kfold = KFold(n_splits=N_FOLDS, shuffle=False)
@@ -47,11 +51,9 @@ chan_volt['beryl_acronyms'] = br.get(br.id[br.mappings['Beryl'][inds]])['acronym
 print('Decoding brain regions..')
 region_predict = np.empty(chan_volt.shape[0]).astype(object)
 feature_imp = np.empty((N_FOLDS, len(FEATURES)))
-for i, (train_index, test_index) in zip(np.arange(N_FOLDS), kfold.split(feature_arr)):
-    print(f'Fold {i+1} of {N_FOLDS}')
-    clf.fit(feature_arr[train_index], chan_volt['beryl_acronyms'].values[train_index])
-    region_predict[test_index] = clf.predict(feature_arr[test_index])
-    feature_imp[i, :] = clf.feature_importances_
+train_index, test_index = kfold.split(feature_arr)
+clf.fit(feature_arr[train_index], chan_volt['beryl_acronyms'].values[train_index])
+region_predict[test_index] = clf.predict(feature_arr[test_index])
 acc = accuracy_score(chan_volt['beryl_acronyms'].values, region_predict)
 print(f'Accuracy: {acc*100:.1f}%')
 print(f'Chance level: {(1/chan_volt["beryl_acronyms"].unique().shape[0])*100:.1f}%')
