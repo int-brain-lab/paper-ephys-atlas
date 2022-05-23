c78e07016c741dda432d5c6e5d89136a71411f40
Guido Meijer <gtmeijer@gmail.com>
2022-05-23 10:13:47 +0100

    Update to new loading function

--- sources/decoding/load_model.py
+++ sources/decoding/load_model.py
@@ -7,7 +7,7 @@ Created on Sat May 21 17:05:48 2022
 
 import numpy as np
 import pandas as pd
-from os.path import join
+from os.path import join, dirname, realpath, split
 import pathlib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -19,19 +19,19 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Settings
-PATH = 'C:\\Users\\guido\\Data\\EphysAtlas'
 FEATURES = ['psd_delta', 'psd_theta', 'psd_alpha', 'psd_beta', 'psd_gamma', 'rms_ap', 'rms_lf',
             'spike_rate', 'axial_um', 'x', 'y', 'depth', 'theta', 'phi']
 #PID = '64d04585-67e7-4320-baad-8d4589fd18f7'
 PID = '31d8dfb1-71fd-4c53-9229-7cd48bee07e4'
 
 # Load in data
-data = load_channel_data(PATH)
+data = load_channel_data()
 data = data[data.index == PID]
 feature_arr = data[FEATURES].to_numpy()
 
 # Load in model
-clf = load(join(PATH, 'model.pkl'))
+path = join(split(dirname(realpath(__file__)))[0], 'trained_models')
+clf = load(join(path, 'model.pkl'))
 region_predict = clf.predict(feature_arr)
 
 
