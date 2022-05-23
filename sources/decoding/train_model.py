e4fa67d7b8158d9a3bd18a4c88249fcaf3fd8b8a
Guido Meijer <gtmeijer@gmail.com>
2022-05-23 15:13:13 +0100

    More atlas functionality

--- sources/decoding/train_model.py
+++ sources/decoding/train_model.py
@@ -18,7 +18,10 @@ br = BrainRegions()
 parser = argparse.ArgumentParser()
 
 # Settings
-parser.add_argument("-data_path", "--data_path", help="Path to training data")
+parser.add_argument("-atlas", "--atlas", help="allen, beryl or cosmos")
+parser.add_argument("-max_depth", "--max_depth", help="Max depth")
+parser.add_argument("-n_trees", "--n_trees", help="Number of trees")
+parser.add_argument("-max_leaf_nodes", "--max_leaf_nodes", help="Max leaf node")
 args = parser.parse_args()
 PID_EXCL = ['64d04585-67e7-4320-baad-8d4589fd18f7', '31d8dfb1-71fd-4c53-9229-7cd48bee07e4']
 FEATURES = ['psd_delta', 'psd_theta', 'psd_alpha', 'psd_beta', 'psd_gamma', 'rms_ap', 'rms_lf',
@@ -30,17 +33,16 @@ merged_df = merged_df.drop(PID_EXCL, axis=0)  # drop PIDs used for evaluation
 feature_arr = merged_df[FEATURES].to_numpy()
 
 # Initialize
-clf = RandomForestClassifier(random_state=42, n_estimators=40, max_depth=20, max_leaf_nodes=15000,
+clf = RandomForestClassifier(random_state=42, n_estimators=int(args.n_trees),
+                             max_depth=int(args.max_depth),
+                             max_leaf_nodes=int(args.max_leaf_nodes),
                              n_jobs=-1, class_weight='balanced')
-
-# Remap to Beryl atlas
-_, inds = ismember(br.acronym2id(merged_df['acronym']), br.id[br.mappings['Allen']])
-merged_df['beryl_acronyms'] = br.get(br.id[br.mappings['Beryl'][inds]])['acronym']
 
 # Fit model
 print('Fitting model..')
-clf.fit(feature_arr, merged_df['beryl_acronyms'].values)
+clf.fit(feature_arr, merged_df['f{args.atlas}_acronyms'].values)
 
 # Save fitted model to disk
-dump(clf, join(split(pathlib.Path(__file__).parent.resolve())[0], 'trained_models', 'channels_model.pkl'), compress=3)
+dump(clf, join(split(pathlib.Path(__file__).parent.resolve())[0], 'trained_models',
+               f'channels_model_{args.atlas}.pkl'), compress=3)
 print('Fitted model saved to disk')
