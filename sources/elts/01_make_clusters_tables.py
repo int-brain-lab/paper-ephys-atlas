272199ab9a9319f84c4de02a2d0f323f5baffde7
owinter <olivier.winter@hotmail.fr>
2022-08-12 10:42:42 +0200

    Probabilistic brain atlas - brain wide map cluster table ELT

--- sources/elts/01_make_clusters_tables.py
+++ sources/elts/01_make_clusters_tables.py
@@ -9,16 +9,18 @@ from neuropixel import trace_header
 from iblutil.numerical import ismember2d
 from ephys_atlas.data import bwm_pids
 from brainbox.io.one import SpikeSortingLoader
+from iblutil.util import get_logger
 
+logger = get_logger('ibl')
 one = ONE()
 ba = AllenAtlas()
 
 
-STAGING_PATH = Path('/datadisk/FlatIron/tables/bwm')
-STAGING_PATH = Path('/mnt/s0/Data/tables/bwm')
-
+STAGING_PATH = Path('/datadisk/FlatIron/tables/atlas')
+STAGING_PATH = Path('/mnt/s0/Data/tables/atlas')
 
 excludes = []
+errorkey = []
 error404 = []
 one = ONE(base_url='https://alyx.internationalbrainlab.org')
 pids, _ = bwm_pids(one, tracing=True)
@@ -40,7 +42,7 @@ for i, pid in enumerate(pids):
     df_probes['pname'][i] = pname
 
     # spikes, clusters, channels = load_spike_sorting_fast(eid=eid, probe=pname, one=one, nested=False)
-    print(i, pid)
+    logger.info(f"{i}, {pid}")
 
     ss = SpikeSortingLoader(pid=pid, one=one, atlas=ba)
     import urllib.error
@@ -48,6 +50,11 @@ for i, pid in enumerate(pids):
         spikes, clusters, channels = ss.load_spike_sorting()
     except urllib.error.HTTPError:
         error404.append(pid)
+        logger.error(f"{pid} error 404")
+        continue
+    except KeyError:
+        errorkey.append(pid)
+        logger.error(f"{pid} key error")
         continue
     clusters = ss.merge_clusters(spikes, clusters, channels)
     df_probes['spike_sorter'][i] = ss.collection
@@ -76,3 +83,11 @@ STAGING_PATH.mkdir(exist_ok=True, parents=True)
 df_channels.to_parquet(STAGING_PATH.joinpath('channels.pqt'))
 df_clusters.to_parquet(STAGING_PATH.joinpath('clusters.pqt'))
 df_probes.to_parquet(STAGING_PATH.joinpath('probes.pqt'))
+
+
+print(f'aws s3 sync "{STAGING_PATH}" s3://ibl-brain-wide-map-private/data/tables/atlas')
+print(errorkey)
+print(error404)
+# run 12-AUG-2022:
+# ['6358854e-51d2-47de-a278-5cbfd155feb6', '9991277b-9e8e-4db8-b8a6-e2c0176ef9ad', 'e070023a-2a5d-4398-ad66-f9ed5d517ad1', '5e84c8e7-236f-4a64-a944-dc4a17c64f1d', '43d4f889-4b05-44df-8306-ea10f854776f']
+# ['de5d704c-4a5b-4fb0-b4af-71614c510a8b', '04db6b9e-a80c-4507-a98e-ad76294ac444', '531423f6-d36d-472b-8234-c8f7b8293f79']
