add00e90305fd7287f0a91322e2633a92c4f7155
owinter <olivier.winter@hotmail.fr>
2022-08-23 12:45:00 +0100

    add depths table in download tables examples

--- sources/examples/00_load_clusters_tables.py
+++ sources/examples/00_load_clusters_tables.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from pathlib import Path
 from one.remote import aws
 from one.api import ONE
@@ -10,3 +12,21 @@ s3, bucket_name = aws.get_s3_from_alyx(alyx=one.alyx)
 aws.s3_download_folder("data/tables/atlas",
                        LOCAL_DATA_PATH,
                        s3=s3, bucket_name=bucket_name)
+
+clusters = pd.read_parquet(LOCAL_DATA_PATH.joinpath('clusters.pqt'))
+probes = pd.read_parquet(LOCAL_DATA_PATH.joinpath('probes.pqt'))
+channels = pd.read_parquet(LOCAL_DATA_PATH.joinpath('channels.pqt'))
+depths = pd.read_parquet(LOCAL_DATA_PATH.joinpath('depths.pqt'))
+
+
+## %%
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.figure()
+sns.countplot(data=probes, x='histology', palette='deep')
+
+plt.figure()
+sns.countplot(data=clusters, x='label',  palette='deep')
+
+## %%
+
