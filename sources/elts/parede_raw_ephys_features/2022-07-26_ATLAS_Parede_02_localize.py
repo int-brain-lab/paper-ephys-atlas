e57ee35e759de3768234f31cb8ffe3a54be465c3
Olivier Winter <olivier.winter@hotmail.fr>
2022-10-28 17:52:23 +0100

    rawephys features computation functions

--- sources/elts/parede_raw_ephys_features/2022-07-26_ATLAS_Parede_02_localize.py
+++ sources/elts/parede_raw_ephys_features/2022-07-26_ATLAS_Parede_02_localize.py
@@ -1,70 +1,27 @@
-"""
-Meant to be run on parede
-cd /home/ibladmin/Documents/PYTHON/SPIKE_SORTING
-conda activate pyks2
-"""
-
 from pathlib import Path
-import numpy as np
-import pandas as pd
+import time
 import torch
 
-from spike_psvae.subtract import make_channel_index, subtract_and_localize_numpy
-from neurodsp.utils import rms
-import neuropixel
 from ibllib.misc import check_nvidia_driver
 from iblutil.util import get_logger
-from neurodsp.utils import WindowGenerator
 
+_logger = get_logger('ephys_atlas', level='INFO')
+
+VERSION = '1.0.0'
+ROOT_PATH = Path("/mnt/s1/ephys-atlas")
+LFP_RESAMPLE_FACTOR = 10  # 200 Hz data
+
+check_nvidia_driver()
 print(torch.version.cuda)
 assert torch.cuda.is_available(), "CUDA not available"
-check_nvidia_driver()
 
-ROOT_PATH = Path("/mnt/s0/ephys-atlas")
-# ROOT_PATH = Path("/datadisk/scratch/ephys-atlas")
-logger = get_logger('ibllib', file=ROOT_PATH.joinpath('localisation_log.txt'))
-
-h = neuropixel.trace_header(version=1)
-geom = np.c_[h['x'], h['y']]
-xy = h['x'] + 1j * h['y']
-
-kwargs = dict(
-    extract_radius=200.,
-    loc_radius = 100.,
-    dedup_spatial_radius = 70.,
-    thresholds=[12, 10, 8, 6, 5],
-    radial_parents=None,
-    tpca=None,
-    device=None,
-    probe="np1",
-    trough_offset=42,
-    spike_length_samples=121,
-    loc_workers=1
-)
-channel_index = make_channel_index(geom, kwargs['extract_radius'], distance_order=False)
+ROOT_PATH = Path("/mnt/s1/ephys-atlas")
 
 
-all_files = list(ROOT_PATH.rglob('chunk*_destripe.npy'))
-for i, file_destripe in enumerate(all_files):
-    file_waveforms = Path(str(file_destripe).replace('_destripe.npy', '_waveforms.npy'))
-    file_spikes = Path(str(file_destripe).replace('_destripe.npy', '_spikes.pqt'))
-    if file_waveforms.exists() and file_spikes.exists():
-        continue
-    logger.info(f"{i}/{len(all_files)}: {file_destripe}")
-    data = np.load(file_destripe).astype(np.float32)
-    # here the normalisation is based off a single chunk, but should this be constant for the whole recording ?
-    data = data / rms(data, axis=-1)[:, np.newaxis]
-    wg = WindowGenerator(data.shape[-1], 30000, overlap=0)
-    localisation = []
-    try:
-        for first, last in wg.firstlast:
-            loc, wfs = subtract_and_localize_numpy(data[:, first:last].T, geom, **kwargs)
-            cleaned_wfs = wfs if first == 0 else np.concatenate([cleaned_wfs, wfs], axis=0)
-            localisation.append(loc)
-    except TypeError as e:
-        logger.error(f"type error: {file_destripe}")
-        continue
+for ff in ROOT_PATH.rglob(".localise_me*"):
+    print(f"python ./sources/ephys_atlas/rawephys.py localisation --path {ff.parent}")
 
-    localisation = pd.concat(localisation)
-    np.save(file_waveforms, cleaned_wfs)
-    localisation.to_parquet(file_spikes)
+# conda activate iblenv
+# cd /home/ibladmin/Documents/PYTHON/int-brain-lab/paper-ephys-atlas
+# python ./sources/elts/parede_raw_ephys_features/2022-07-26_ATLAS_Parede_02_localize.py > ~/parede_localise.sh
+# source ~/parede_localise.sh
