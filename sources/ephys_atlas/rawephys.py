a6d9ff85cdc828b0c6e0e9612e7a39dfec4102ca
Olivier Winter <olivier.winter@hotmail.fr>
2022-10-28 17:52:23 +0100

    rawephys features computation functions

--- sources/ephys_atlas/rawephys.py
+++ sources/ephys_atlas/rawephys.py
@@ -1,60 +1,128 @@
+import argparse
 from pathlib import Path
+import sys
+import yaml
 import shutil
+
 import numpy as np
+import scipy.signal
+import pandas as pd
 
+import neuropixel
+from neurodsp import voltage
+from neurodsp.utils import rms
+from brainbox.io.spikeglx import Streamer
 from one.api import ONE
-import spikeglx
-from neurodsp.voltage import decompress_destripe_cbin
-from viewephys.gui import viewephys
 
-one = ONE()
-ROOT_PATH_NEW = Path("/media/olivier/Seagate Expansion Drive/hackathon")
-ROOT_PATH = Path("/datadisk/FlatIron")
-
-# '/media/olivier/Seagate Expansion Drive/hackathon/zadorlab/Subjects/CSH_ZAD_025/2020-08-04/002/raw_ephys_data/probe01/destripe/_spikeglx_ephysData_g0_t0.imec1.lf.bin
-pid = "64d04585-67e7-4320-baad-8d4589fd18f7"  # 280ee768-f7b8-4c6c-9ea0-48ca75d6b6f3	probe01	64d04585-67e7-4320-baad-8d4589fd18f7	zadorlab/Subjects/CSH_ZAD_025/2020-08-04/002 CA1
-pid = "31d8dfb1-71fd-4c53-9229-7cd48bee07e4"  # 493170a6-fd94-4ee4-884f-cc018c17eeb9	probe01	31d8dfb1-71fd-4c53-9229-7cd48bee07e4	hoferlab/Subjects/SWC_061/2020-11-23/001	MOs
-
-# stub = "zadorlab/Subjects/CSH_ZAD_025/2020-08-04/002"
-# print(f'rsync -av --progress "/media/olivier/Seagate Expansion Drive/hackathon/{stub}/" /datadisk/FlatIron/{stub}')
-
-eid, pname = one.pid2eid(pid)
-session_path = one.eid2path(eid)
-session_path = ROOT_PATH_NEW.joinpath(session_path.relative_to(ROOT_PATH))
-proc_path = session_path.joinpath('raw_ephys_data', pname, 'destripe')
-proc_path.mkdir(parents=True, exist_ok=True)
-print(proc_path)
-
-ap_file = next(session_path.joinpath('raw_ephys_data', pname).rglob('*.ap.cbin'))
-lf_file = next(session_path.joinpath('raw_ephys_data', pname).rglob('*.lf.cbin'))
-
-ap_file_destriped = proc_path.joinpath(ap_file.stem + ".bin")
-lf_file_destriped = proc_path.joinpath(lf_file.stem + ".bin")
-
-# sr = spikeglx.Reader(ap_file)
-if not ap_file_destriped.exists():
-    decompress_destripe_cbin(ap_file, output_file=ap_file_destriped, compute_rms=True, reject_channels=True)
-    shutil.copy(ap_file.with_suffix(".meta"), ap_file_destriped.with_suffix(".meta"))
-
-if not lf_file_destriped.exists():
-    decompress_destripe_cbin(lf_file, output_file=lf_file_destriped, compute_rms=True, reject_channels=True,
-                             butter_kwargs={'N': 3, 'Wn': 2 / 2500 * 2, 'btype': 'highpass'}, k_filter=False)
-    shutil.copy(lf_file.with_suffix(".meta"), lf_file_destriped.with_suffix(".meta"))
+from iblutil.util import get_logger
+from neurodsp.utils import WindowGenerator
 
 
-sr_ = spikeglx.Reader(lf_file)
-sr = spikeglx.Reader(lf_file_destriped)
-t0 = 20
+_logger = get_logger('ephys_atlas', level='INFO')
 
-dat = sr[int(t0 * sr.fs): int((t0 + 0.5) * sr.fs), :]
-dat_ = sr_[int(t0 * sr.fs): int((t0 + 0.5) * sr.fs), :]
+LFP_RESAMPLE_FACTOR = 10  # 200 Hz data
+VERSION = '1.0.0'
 
 
-from neurodsp.voltage import destripe
+def destripe(pid, one=None, typ='ap', prefix="", destination=None):
+    """
+    Stream chunks of data from a given probe insertion
 
-viewephys(dat.T, sr.fs, title='destripe')
+    Output folder architecture (the UUID is the probe insertion UUID):
+        f4bd76a6-66c9-41f3-9311-6962315f8fc8_ZFM-02369_2021-05-19_001_probe00
+            ├── chunk0400_lfp.npy
+            ├── chunk0400_lfp.yml
+            ├── chunk0500_destripe.npy
+            ├── chunk0500_destripe.yml
+
+    :param pid:
+    :param one:
+    :param typ:
+    :param prefix:
+    :param destination:
+    :return:
+    """
+    assert one
+    assert destination
+    eid, pname = one.pid2eid(pid)
+
+    if typ == 'ap':
+        sample_duration, sample_spacings, skip_start_end = (10 * 30_000, 1_000 * 30_000, 500 * 30_000)
+        suffix = "destripe"
+    elif typ == 'lf':
+        sample_duration, sample_spacings, skip_start_end = (30 * 2_000, 1_000 * 2_000, 500 * 2_000)
+        suffix = 'lfp'
+    sr = Streamer(pid=pid, one=one, remove_cached=True, typ=typ)
+    chunk_size = sr.chunks['chunk_bounds'][1]
+    nsamples = np.ceil((sr.shape[0] - sample_duration - skip_start_end * 2) / sample_spacings)
+    t0_samples = np.round((np.arange(nsamples) * sample_spacings + skip_start_end) / chunk_size) * chunk_size
+
+    for s0 in t0_samples:
+        # ap: chunk1500_destripe.yml, lf: chunk1200_lfp.npy
+        file_destripe = destination.joinpath(f'{prefix}chunk{str(int(s0 / chunk_size)).zfill(4)}_{suffix}.npy')
+        file_yaml = destination.joinpath(f'{prefix}chunk{str(int(s0 / chunk_size)).zfill(4)}_{suffix}.yml')
+        if file_destripe.exists():
+            continue
+        tsel = slice(int(s0), int(s0) + int(sample_duration))
+        raw = sr[tsel, :-sr.nsync].T
+        if typ == 'ap':
+            destripe = voltage.destripe(raw, fs=sr.fs, neuropixel_version=1, channel_labels=True)
+            fs_out = sr.fs
+        elif typ == 'lf':
+            destripe = voltage.destripe_lfp(raw, fs=sr.fs, neuropixel_version=1, channel_labels=True)
+            destripe = scipy.signal.decimate(destripe, LFP_RESAMPLE_FACTOR, axis=1, ftype='fir')
+            fs_out = sr.fs / LFP_RESAMPLE_FACTOR
+        # destination example: PosixPath('/mnt/s1/ephys-atlas/06d42449-d6ac-4c35-8f85-24ecfbc08bc1_ibl_witten_29_2021-06-10_003_probe00')
+        destination.mkdir(exist_ok=True)
+        np.save(file_destripe, destripe.astype(np.float16))
+        with open(file_yaml, 'w+') as fp:
+            yaml.dump(dict(fs=fs_out, eid=eid, pid=pid, pname=pname, nc=raw.shape[0], dtype="float16"), fp)
 
 
-viewephys(dat_.T, sr.fs, title='raw')
-viewephys(destripe(dat_.T[:-1, :], sr.fs), sr.fs, title='destripe_')
+def localisation(destination=None):
+    """
+    :return:
+    """
+    from spike_psvae.subtract import make_channel_index, subtract_and_localize_numpy
+    h = neuropixel.trace_header(version=1)
+    geom = np.c_[h['x'], h['y']]
+    xy = h['x'] + 1j * h['y']
 
+    kwargs = dict(
+        extract_radius=200.,
+        loc_radius=100.,
+        dedup_spatial_radius=70.,
+        thresholds=[12, 10, 8, 6, 5],
+        radial_parents=None,
+        tpca=None,
+        device=None,
+        probe="np1",
+        trough_offset=42,
+        spike_length_samples=121,
+        loc_workers=1
+    )
+    # channel_index = make_channel_index(geom, kwargs['extract_radius'], distance_order=False)
+
+    all_files = list(destination.rglob('chunk*_destripe.npy'))
+    for i, file_destripe in enumerate(all_files):
+        file_waveforms = Path(str(file_destripe).replace('_destripe.npy', '_waveforms.npy'))
+        file_spikes = Path(str(file_destripe).replace('_destripe.npy', '_spikes.pqt'))
+        if file_waveforms.exists() and file_spikes.exists():
+            continue
+        _logger.info(f"{i}/{len(all_files)}: {file_destripe}")
+        data = np.load(file_destripe).astype(np.float32)
+        # here the normalisation is based off a single chunk, but should this be constant for the whole recording ?
+        data = data / rms(data, axis=-1)[:, np.newaxis]
+        wg = WindowGenerator(data.shape[-1], 30000, overlap=0)
+        localisation = []
+        try:
+            for first, last in wg.firstlast:
+                loc, wfs = subtract_and_localize_numpy(data[:, first:last].T, geom, **kwargs)
+                cleaned_wfs = wfs if first == 0 else np.concatenate([cleaned_wfs, wfs], axis=0)
+                localisation.append(loc)
+        except TypeError as e:
+            _logger.error(f"type error: {file_destripe}")
+            continue
+        localisation = pd.concat(localisation)
+        np.save(file_waveforms, cleaned_wfs)
+        localisation.to_parquet(file_spikes)
--- sources/examples/allen_dataset_features.py
+++ sources/examples/allen_dataset_features.py
@@ -9,12 +9,12 @@ nwb = h5.File(spikes_nwb_file, 'r')
 
 ## nwb.visit(print)
 
+results = {}
 for probe in nwb['processing'].keys():
     st = nwb['processing'][probe]['Clustering']['times'][()]
     sc = nwb['processing'][probe]['Clustering']['num'][()]
     sa = nwb['processing'][probe]['Clustering']['peak_over_rms'][()]
     cdx = nwb['processing'][probe]['Clustering']['cluster_nums'][()]
-
 
     srp = slidingRP.metrics.slidingRP_all(spikeTimes=st, spikeClusters=sc,
                                           **{'sampleRate': 30000, 'binSizeCorr': 1 / 30000})
@@ -28,18 +28,31 @@ for probe in nwb['processing'].keys():
         _, noise_cutoff[ic], _ = brainbox.metrics.single_units.noise_cutoff(sa[sel], **params['noise_cutoff'])
         amp_median[ic] = np.median(sa[sel])
 
-
     labels = np.c_[
         np.array(slidingRP_viol),
         noise_cutoff < params['noise_cutoff']['nc_threshold'],
         amp_median > params['med_amp_thresh_uv'] / 1e6,
     ]
     ngood = np.sum(np.sum(labels, axis=-1) == 3)
-    print(probe,  ngood, cdx.size, ngood / cdx.size * 100)
 
-# probeA 58 304 19.078947368421055
-# probeB 60 300 20.0
-# probeC 94 508 18.503937007874015
-# probeD 31 362 8.56353591160221
-# probeE 74 385 19.22077922077922
-# probeF 45 268 16.791044776119403
+    clusters = dict(ind=nwb['processing'][probe]['unit_list'][()])
+    # clusters['label'] = np.zeros_like(clusters['ind'])
+    # for ic in clusters['ind']:
+    #     nwb['processing'][probe]['UnitTimes'][str(ic)].keys()
+    #     print(ic)
+
+    results[probe] = dict(n_ibl=ngood, n=cdx.size, n_allen= clusters['ind'].size)
+    break
+
+import pandas as pd
+res = pd.DataFrame(results).T
+res['p_allen'] = res['n_allen'] / res['n'] * 100
+res['p_ibl'] = res['n_ibl'] / res['n'] * 100
+
+#         n_ibl    n  n_allen    p_allen      p_ibl
+# probeA     58  304      250  82.236842  19.078947
+# probeB     60  300      231  77.000000  20.000000
+# probeC     94  508      379  74.606299  18.503937
+# probeD     31  362      203  56.077348   8.563536
+# probeE     74  385      301  78.181818  19.220779
+# probeF     45  268      182  67.910448  16.791045
