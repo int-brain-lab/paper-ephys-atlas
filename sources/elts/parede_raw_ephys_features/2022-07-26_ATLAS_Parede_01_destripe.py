f1bb4f3ea45e5b59055ad7bfa3223e6c6b85494b
Olivier Winter <olivier.winter@hotmail.fr>
2022-10-28 17:52:23 +0100

    rawephys features computation functions

---
+++ sources/elts/parede_raw_ephys_features/2022-07-26_ATLAS_Parede_01_destripe.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from one.api import ONE
+import shutil
+from ephys_atlas.data import bwm_pids
+import ephys_atlas.rawephys
+
+
+VERSION = '1.0.0'
+ROOT_PATH = Path("/mnt/s1/ephys-atlas")
+LFP_RESAMPLE_FACTOR = 10  # 200 Hz data
+
+one = ONE(base_url="https://alyx.internationalbrainlab.org")
+pids, alyx_pids = bwm_pids(one)
+
+c = 0
+for i, apid in enumerate(alyx_pids):
+    pid = apid['id']
+    eid = apid['session_info']['id']
+    pname = apid['name']
+    stub = [apid['session_info']['subject'], apid['session_info']['start_time'][:10], f"{apid['session_info']['number']:03d}"]
+    destination = ROOT_PATH.joinpath('_'.join(([pid] + stub + [pname])))
+    if destination.is_dir():
+        if destination.joinpath(f'.version_{VERSION}').exists() or destination.joinpath(f'.01_destripe_{VERSION}').exists():
+            continue
+    print(i, f"python ./sources/ephys_atlas/rawephys.py destripe --pid {pid} --path {destination}")
+    c += 1
+    shutil.rmtree(destination, ignore_errors=True)
+    ephys_atlas.rawephys.destripe(pid, one=one, destination=destination)
+    destination.joinpath(f'.01_destripe_{VERSION}').touch()
