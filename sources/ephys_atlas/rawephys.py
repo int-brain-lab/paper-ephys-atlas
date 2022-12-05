08aa854e168d403033892bc19f6d387ca7f4d481
Olivier Winter <olivier.winter@hotmail.fr>
2022-12-05 15:31:19 +0000

    Update features computations

--- sources/ephys_atlas/rawephys.py
+++ sources/ephys_atlas/rawephys.py
@@ -17,19 +17,22 @@ from neurodsp.utils import WindowGenerator
 _logger = get_logger('ephys_atlas', level='INFO')
 
 LFP_RESAMPLE_FACTOR = 10  # 200 Hz data
-VERSION = '1.0.0'
+VERSION = '1.1.0'
 
 
-def destripe(pid, one=None, typ='ap', prefix="", destination=None, remove_cached=True):
+def destripe(pid, one=None, typ='ap', prefix="", destination=None, remove_cached=True, clobber=False):
     """
     Stream chunks of data from a given probe insertion
 
     Output folder architecture (the UUID is the probe insertion UUID):
-        f4bd76a6-66c9-41f3-9311-6962315f8fc8_ZFM-02369_2021-05-19_001_probe00
-            ├── chunk0400_lfp.npy
-            ├── chunk0400_lfp.yml
-            ├── chunk0500_destripe.npy
-            ├── chunk0500_destripe.yml
+        f4bd76a6-66c9-41f3-9311-6962315f8fc8
+        ├── T00500
+        │   ├── ap.npy
+        │   ├── ap.yml
+        │   ├── lf.npy
+        │   ├── lf.yml
+        │   ├── spikes.pqt
+        │   └── waveforms.npy
 
     :param pid:
     :param one:
@@ -42,44 +45,48 @@ def destripe(pid, one=None, typ='ap', prefix="", destination=None, remove_cached
     assert destination
     eid, pname = one.pid2eid(pid)
 
+    butter_kwargs = {'N': 3, 'Wn': 300 / 30000 * 2, 'btype': 'highpass'}
+    sos = scipy.signal.butter(**butter_kwargs, output='sos')
+
     if typ == 'ap':
         sample_duration, sample_spacings, skip_start_end = (10 * 30_000, 1_000 * 30_000, 500 * 30_000)
-        suffix = "destripe"
     elif typ == 'lf':
         sample_duration, sample_spacings, skip_start_end = (20 * 2_500, 1_000 * 2_500, 500 * 2_500)
-        suffix = 'lfp'
     sr = Streamer(pid=pid, one=one, remove_cached=remove_cached, typ=typ)
     chunk_size = sr.chunks['chunk_bounds'][1]
     nsamples = np.ceil((sr.shape[0] - sample_duration - skip_start_end * 2) / sample_spacings)
     t0_samples = np.round((np.arange(nsamples) * sample_spacings + skip_start_end) / chunk_size) * chunk_size
 
     for s0 in t0_samples:
-        # ap: chunk1500_destripe.yml, lf: chunk1200_lfp.npy
-        file_destripe = destination.joinpath(f'{prefix}chunk{str(int(s0 / chunk_size)).zfill(4)}_{suffix}.npy')
-        file_yaml = destination.joinpath(f'{prefix}chunk{str(int(s0 / chunk_size)).zfill(4)}_{suffix}.yml')
-        if file_destripe.exists():
+        t0 = int(s0 / chunk_size)
+        file_destripe = destination.joinpath(f"T{t0:05d}", f"{typ}.npy")
+        file_yaml = file_destripe.with_suffix('.yml')
+        if file_destripe.exists() and clobber is False:
             continue
         tsel = slice(int(s0), int(s0) + int(sample_duration))
         raw = sr[tsel, :-sr.nsync].T
         if typ == 'ap':
             destripe = voltage.destripe(raw, fs=sr.fs, neuropixel_version=1, channel_labels=True)
+            # saves a 0.05 secs snippet of the butterworth filtered data at 0.5sec offset for QC purposes
+            butt = scipy.signal.sosfiltfilt(sos, raw)[:, int(sr.fs * 0.5):int(sr.fs * 0.55)]
             fs_out = sr.fs
         elif typ == 'lf':
             destripe = voltage.destripe_lfp(raw, fs=sr.fs, neuropixel_version=1, channel_labels=True)
             destripe = scipy.signal.decimate(destripe, LFP_RESAMPLE_FACTOR, axis=1, ftype='fir')
             fs_out = sr.fs / LFP_RESAMPLE_FACTOR
-        # destination example: PosixPath('/mnt/s1/ephys-atlas/06d42449-d6ac-4c35-8f85-24ecfbc08bc1_ibl_witten_29_2021-06-10_003_probe00')
-        destination.mkdir(exist_ok=True)
+        file_destripe.parent.mkdir(exist_ok=True)
         np.save(file_destripe, destripe.astype(np.float16))
         with open(file_yaml, 'w+') as fp:
             yaml.dump(dict(fs=fs_out, eid=eid, pid=pid, pname=pname, nc=raw.shape[0], dtype="float16"), fp)
+        if typ == 'ap':
+            np.save(file_destripe.parent.joinpath('raw.npy'), butt.astype(np.float16))
 
 
-def localisation(destination=None):
+def localisation(destination=None, clobber=False):
     """
     :return:
     """
-    from spike_psvae.subtract import make_channel_index, subtract_and_localize_numpy
+    from spike_psvae.subtract import subtract_and_localize_numpy
     h = neuropixel.trace_header(version=1)
     geom = np.c_[h['x'], h['y']]
 
@@ -98,14 +105,16 @@ def localisation(destination=None):
     )
     # channel_index = make_channel_index(geom, kwargs['extract_radius'], distance_order=False)
 
-    all_files = list(destination.rglob('chunk*_destripe.npy'))
-    for i, file_destripe in enumerate(all_files):
-        file_waveforms = Path(str(file_destripe).replace('_destripe.npy', '_waveforms.npy'))
-        file_spikes = Path(str(file_destripe).replace('_destripe.npy', '_spikes.pqt'))
-        if file_waveforms.exists() and file_spikes.exists():
+    all_files = list(destination.rglob('ap.npy'))
+    for i, ap_file in enumerate(all_files):
+        chunk_dir = ap_file.parent
+        file_waveforms = chunk_dir.joinpath('waveforms.npy')
+        file_spikes = chunk_dir.joinpath('spikes.pqt')
+        if file_waveforms.exists() and file_spikes.exists() and clobber is False:
+            _logger.info(f"{i}/{len(all_files)}: {ap_file}, SKIP")
             continue
-        _logger.info(f"{i}/{len(all_files)}: {file_destripe}")
-        data = np.load(file_destripe).astype(np.float32)
+        _logger.info(f"{i}/{len(all_files)}: {ap_file}, COMPUTE")
+        data = np.load(ap_file).astype(np.float32)
         # here the normalisation is based off a single chunk, but should this be constant for the whole recording ?
         data = data / rms(data, axis=-1)[:, np.newaxis]
         wg = WindowGenerator(data.shape[-1], 30000, overlap=0)
@@ -114,9 +123,10 @@ def localisation(destination=None):
             for first, last in wg.firstlast:
                 loc, wfs = subtract_and_localize_numpy(data[:, first:last].T, geom, **kwargs)
                 cleaned_wfs = wfs if first == 0 else np.concatenate([cleaned_wfs, wfs], axis=0)
+                loc['sample'] += first
                 localisation.append(loc)
         except (TypeError, ValueError):
-            _logger.error(f"type error: {file_destripe}")
+            _logger.error(f"type error: {ap_file}")
             continue
         localisation = pd.concat(localisation)
         np.save(file_waveforms, cleaned_wfs)
