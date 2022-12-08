e3f5c08253947e37ace079d6e4dd6472562e1b7f
Gaelle <gaelle.chapuis.ibl@gmail.com>
2022-12-08 14:38:52 +0100

    dummy way to plot phase ; compute not working

--- sources/scripts/GC__LF_Spike_Coherence.py
+++ sources/scripts/GC__LF_Spike_Coherence.py
@@ -11,6 +11,7 @@ from ibllib.atlas import AllenAtlas
 from scipy import signal
 from viewephys.gui import viewephys
 import numpy as np
+from numpy import angle
 
 one = ONE()
 ba = AllenAtlas()
@@ -30,7 +31,7 @@ pids = [
     'eebcaf65-7fa4-4118-869d-a084e84530e2',
     'fe380793-8035-414e-b000-09bfe5ece92a',
 ]
-pid = pids[5]
+pid = pids[0]
 
 # == LOAD LFP
 
@@ -49,10 +50,13 @@ destriped_LFP = destripe(raw, fs=sr.fs)
 
 # == Band-pass LF
 fNQ = sr.fs/2
-Wn = [5, 10]                          # Set the passband [5-7] Hz,
+Wn = [31, 33]                          # Set the passband [5-7] Hz,
 n = 100                             # ... and filter order,
 b = signal.firwin(n, Wn, nyq=fNQ, pass_zero=False, window='hamming')
 v_filt = signal.filtfilt(b, 1, destriped_LFP)    # ... and apply it to the data.
+
+# compute phase
+phi = angle(signal.hilbert(v_filt))
 
 # == LOAD SPIKES
 sl = SpikeSortingLoader(pid=pid, one=one, atlas=ba)
@@ -73,6 +77,71 @@ c = clusters.channels[spikes.clusters[slice_spikes]]
 eqc = dict()
 
 eqc['filtered'] = viewephys(v_filt, fs=sr.fs)
+eqc['filtered'].ctrl.add_scatter(t, c, rgb=(255, 0, 50), label='all')
+
+eqc['angle'] = viewephys(phi, fs=sr.fs)
+eqc['angle'].ctrl.add_scatter(t, c, rgb=(255, 0, 50), label='all')
+
 eqc['destripe'] = viewephys(destriped_LFP, fs=sr.fs)
-# %gui qt
 eqc['destripe'].ctrl.add_scatter(t, c, rgb=(255, 0, 50), label='all')
+
+# %gui qt
+
+
+# Plot colored line
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
+
+
+# Create a set of line segments so that we can color them individually
+# This creates the points as a N x 1 x 2 array so that we can stack points
+# together easily to get the segments. The segments array for line collection
+# needs to be (numlines) x (points per line) x 2 (for x and y)
+def plt_phase_axis(x, y, phasey, fig, axs, axs_id):
+    # Create a continuous norm to map from data points to colors
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+    norm = plt.Normalize(phasey.min(), phasey.max())
+    lc = LineCollection(segments, cmap='hsv', norm=norm)
+    # Set the values used for colormapping
+    lc.set_array(phasey)
+    lc.set_linewidth(2)
+    line = axs[axs_id].add_collection(lc)
+    fig.colorbar(line, ax=axs[axs_id])
+
+    axs[axs_id].set_xlim(x.min(), x.max())
+    axs[axs_id].set_ylim(y.min(), y.max())
+    # plt.clim(-np.pi, np.pi)
+    plt.show()
+
+# Plot 1 channel LFP
+ch_plt = 10  # 55
+smpl_start = int(500/1000 * sr.fs)
+smpl_end = int(700/1000 * sr.fs)
+v_plt = v_filt[ch_plt, smpl_start:smpl_end+2]
+phi_plt = phi[ch_plt, smpl_start:smpl_end+2]
+t_plt = np.linspace(0, smpl_end-smpl_start, len(v_plt))
+plt.plot(t_plt, v_plt)            # Plot the LFP data,
+
+# fake datapoint to force axis lim
+phi_plt[-1] = -np.pi
+phi_plt[-2] = np.pi
+v_plt[-1] = 0
+v_plt[-2] = 0
+
+x = t_plt
+y = v_plt
+phasey = phi_plt
+fig, axs = plt.subplots(2, 1, sharex=True, sharey=True)
+
+plt_phase_axis(x, y, phasey, fig, axs, axs_id=0)
+
+
+
+
+
+
+
