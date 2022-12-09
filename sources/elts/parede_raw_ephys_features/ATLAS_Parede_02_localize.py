ab7fe5b0ff76d60d909f332e632cf4d12dbb0ff6
Olivier Winter <olivier.winter@hotmail.fr>
2022-12-09 14:29:57 +0000

    elts on parede

--- sources/elts/parede_raw_ephys_features/ATLAS_Parede_02_localize.py
+++ sources/elts/parede_raw_ephys_features/ATLAS_Parede_02_localize.py
@@ -21,21 +21,21 @@ assert torch.cuda.is_available(), "CUDA not available"
 one = ONE(base_url="https://alyx.internationalbrainlab.org")
 pids, alyx_pids = atlas_pids(one)
 
-pids = [
-    '1a276285-8b0e-4cc9-9f0a-a3a002978724',
-    '1e104bf4-7a24-4624-a5b2-c2c8289c0de7',
-    '5d570bf6-a4c6-4bf1-a14b-2c878c84ef0e',
-    '5f7766ce-8e2e-410c-9195-6bf089fea4fd',
-    '6638cfb3-3831-4fc2-9327-194b76cf22e1',
-    '749cb2b7-e57e-4453-a794-f6230e4d0226',
-    'd7ec0892-0a6c-4f4f-9d8f-72083692af5c',
-    'da8dfec1-d265-44e8-84ce-6ae9c109b8bd',
-    'dab512bd-a02d-4c1f-8dbc-9155a163efc0',
-    'dc7e9403-19f7-409f-9240-05ee57cb7aea',
-    'e8f9fba4-d151-4b00-bee7-447f0f3e752c',
-    'eebcaf65-7fa4-4118-869d-a084e84530e2',
-    'fe380793-8035-414e-b000-09bfe5ece92a',
-]
+# pids = [
+#     '1a276285-8b0e-4cc9-9f0a-a3a002978724',
+#     '1e104bf4-7a24-4624-a5b2-c2c8289c0de7',
+#     '5d570bf6-a4c6-4bf1-a14b-2c878c84ef0e',
+#     '5f7766ce-8e2e-410c-9195-6bf089fea4fd',
+#     '6638cfb3-3831-4fc2-9327-194b76cf22e1',
+#     '749cb2b7-e57e-4453-a794-f6230e4d0226',
+#     'd7ec0892-0a6c-4f4f-9d8f-72083692af5c',
+#     'da8dfec1-d265-44e8-84ce-6ae9c109b8bd',
+#     'dab512bd-a02d-4c1f-8dbc-9155a163efc0',
+#     'dc7e9403-19f7-409f-9240-05ee57cb7aea',
+#     'e8f9fba4-d151-4b00-bee7-447f0f3e752c',
+#     'eebcaf65-7fa4-4118-869d-a084e84530e2',
+#     'fe380793-8035-414e-b000-09bfe5ece92a',
+# ]
 
 IMIN = 0
 for i, pid in enumerate(pids):
