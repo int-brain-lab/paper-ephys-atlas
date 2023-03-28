aef6502472009b1f2fc9f38f72188e2e814c0536
Olivier Winter <olivier.winter@hotmail.fr>
2023-03-28 10:12:26 +0100

    destripe / localize workflows

--- sources/elts/parede_raw_ephys_features/ATLAS_Parede_02_localize.py
+++ sources/elts/parede_raw_ephys_features/ATLAS_Parede_02_localize.py
@@ -1,29 +1,24 @@
 from pathlib import Path
-from one.api import ONE
-from ephys_atlas.data import atlas_pids
-import ephys_atlas.workflow as workflow
 
 import torch
 
+from one.api import ONE
 from ibllib.misc import check_nvidia_driver
-from iblutil.util import setup_logger
+from ephys_atlas.data import atlas_pids
+import ephys_atlas.workflow as workflow
 
-_logger = setup_logger('ephys_atlas', level='INFO')
-
-VERSION = '1.2.0'
 ROOT_PATH = Path("/mnt/s1/ephys-atlas")
-LFP_RESAMPLE_FACTOR = 10  # 200 Hz data
 
 check_nvidia_driver()
-print(torch.version.cuda)
 assert torch.cuda.is_available(), "CUDA not available"
+print(torch.version.cuda)
 
 one = ONE(base_url="https://alyx.internationalbrainlab.org")
 pids, alyx_pids = atlas_pids(one)
 
-flow = workflow.report()
+flow = workflow.report(one=one)
+# re-runs all old and error tasks
 pids = flow.index[flow['localise'] != f".localise_{workflow.TASKS['localise']['version']}"]
 for i, pid in enumerate(pids):
     print(i, len(pids))
     workflow.localise(pid)
-
