a4dcbeb8a1e8ddbb306344235163bbd0fe6d7f27
olivier <olivier.winter@hotmail.fr>
2023-05-12 22:32:54 +0100

    create config files for decoder parameters

--- sources/decoding/raw_ephys_decodes_regions.py
+++ sources/decoding/raw_ephys_decodes_regions.py
@@ -2,32 +2,29 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 from ibllib.atlas import BrainRegions
+import ephys_atlas.data
 
-regions = BrainRegions()
-OLD = False
-OUT_PATH = Path("/datadisk/gdrive/2022/08_ephys_atlas_features")
 pd.set_option('use_inf_as_na', True)
-x_list = ['rms_ap', 'alpha_mean', 'alpha_std', 'cloud_x_std', 'cloud_y_std', 'cloud_z_std', 'rms_lf', 'psd_delta', 'psd_theta', 'psd_alpha', 'psd_beta', 'psd_gamma']
-if OLD:
-    LOCAL_DATA_PATH = Path("/datadisk/Data/aggregates/bwm")
-    LOCAL_DATA_PATH = Path("/mnt/s0/aggregates/bwm/2022_W34")
+x_list = ['rms_ap', 'alpha_mean', 'alpha_std', 'cloud_x_std', 'cloud_y_std', 'cloud_z_std',
+          'rms_lf', 'psd_delta', 'psd_theta', 'psd_alpha', 'psd_beta', 'psd_gamma']
+x_list += ['spike_count', 'peak_time_idx', 'peak_val', 'trough_time_idx', 'trough_val', 'tip_time_idx', 'tip_val']
+regions = BrainRegions()
 
-    df_channels = pd.read_parquet(LOCAL_DATA_PATH.joinpath('channels.pqt'))
-    x_list += ['spike_rate']
-else:
-    LOCAL_DATA_PATH = Path("/mnt/s0/aggregates/atlas/2023_W13")
-    df_channels = pd.read_parquet(LOCAL_DATA_PATH.joinpath('channels.pqt'))
-    df_channels.index.rename('channel', level=1, inplace=True)
-    x_list += ['spike_count', 'peak_time_idx', 'peak_val', 'trough_time_idx', 'trough_val', 'tip_time_idx', 'tip_val']
-
-df_voltage = pd.read_parquet(LOCAL_DATA_PATH.joinpath('raw_ephys_features.pqt'))
+## data loading section
+config = ephys_atlas.data.get_config()
+# this path contains channels.pqt, clusters.pqt and raw_ephys_features.pqt
+path_features = Path(config['paths']['features']).joinpath(config['decoding']['tag'])
+df_voltage, df_clusters, df_channels = ephys_atlas.data.load_tables(path_features)
 df_voltage = pd.merge(df_voltage, df_channels, left_index=True, right_index=True).dropna()
+
 aids_cosmos = regions.remap(df_voltage['atlas_id'], source_map='Allen', target_map='Cosmos')
 aids_beryl = regions.remap(df_voltage['atlas_id'], source_map='Allen', target_map='Beryl')
 df_voltage['beryl_id'] = aids_beryl
@@ -37,10 +34,7 @@ X = df_voltage.loc[:, x_list].values
 scaler = StandardScaler()
 scaler.fit(X)
 
-
 def decode(X, scaler, aids, classifier=None, save_path=None):
-    if classifier is None:
-        classifier = MLPClassifier(random_state=1, max_iter=300, verbose=True)
     X_train, X_test, y_train, y_test = train_test_split(X, aids, stratify=aids)
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
@@ -50,15 +44,11 @@ def decode(X, scaler, aids, classifier=None, save_path=None):
     score_null = clf.score(X_test, y_null)
     return clf, score_model, score_null
 
-
- ## %%
-from sklearn.ensemble import RandomForestClassifier
+## %%
 cclas, cs, csn = decode(X, scaler, aids_cosmos, classifier=RandomForestClassifier(verbose=True))
 bclas, bs, bsn = decode(X, scaler, aids_beryl, classifier=RandomForestClassifier(verbose=True))
 print(bs, bsn, cs, csn)
 
-import matplotlib.pyplot as plt
-import seaborn as sns
 sns.set_theme('paper')
 # forest_importances = pd.Series(cclas.feature_importances_, index=x_list)
 # std = np.std([tree.feature_importances_ for tree in cclas.estimators_], axis=0)
