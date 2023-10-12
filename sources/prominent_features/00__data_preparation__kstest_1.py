847bc8e733d27e2d66ae2b4025058d7fab7cf28e
Gaelle <gaelle.chapuis.ibl@gmail.com>
2023-10-12 21:20:57 +0200

    data prep ks test

---
+++ sources/prominent_features/00__data_preparation__kstest_1.py
@@ -0,0 +1,127 @@
+'''
+Code to compute the KS-test, per brain region pair 1-to-1
+'''
+
+from pathlib import Path
+from neurodsp.waveforms import peak_to_trough_ratio
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import seaborn as sns
+from scipy.stats import ks_2samp
+import pandas as pd
+import numpy as np
+
+import ephys_atlas.data
+import ephys_atlas.plots
+from iblatlas.atlas import BrainRegions
+from one.api import ONE
+
+
+one = ONE(mode='remote')
+br = BrainRegions()
+local_path = Path("/Users/gaelle/Documents/Work/EphysAtlas/Data/")
+local_result = local_path.parent.joinpath('Fig3_Result')
+label = '2023_W34'
+USE_DEBIAS = False
+
+# Select brain region level to do analysis over
+brain_id = 'beryl_id'
+
+# Select set of features to do analysis over
+features = ['alpha_mean', 'alpha_std', 'spike_count', 'peak_time_secs', 'peak_val',
+            'trough_time_secs', 'trough_val', 'tip_time_secs', 'tip_val',
+            'recovery_time_secs', 'depolarisation_slope', 'repolarisation_slope',
+            'recovery_slope', 'polarity', 'rms_ap', 'rms_lf', 'psd_delta',
+            'psd_theta', 'psd_alpha', 'psd_beta', 'psd_gamma', 'psd_lfp',
+            'rms_lf_csd', 'psd_delta_csd', 'psd_theta_csd', 'psd_alpha_csd',
+            'psd_beta_csd', 'psd_gamma_csd', 'spike_count_log',
+            'peak_to_trough_duration', 'peak_to_trough_ratio', 'peak_to_trough_ratio_log']
+
+
+##
+# Load and prepare dataframe
+if USE_DEBIAS:
+    _, df_clusters, df_channels, df_probes = ephys_atlas.data.download_tables(
+        local_path, label=label, one=one, verify=True)
+    df_voltage_debias = pd.read_parquet(local_path.joinpath('df_voltage_debias.pqt'))
+    df_voltage = df_voltage_debias
+else:
+    df_voltage, df_clusters, df_channels, df_probes = ephys_atlas.data.download_tables(
+        local_path, label=label, one=one, verify=True)
+
+# Drop nan
+df_voltage = pd.merge(df_voltage, df_channels, left_index=True, right_index=True).dropna()
+
+# Remap
+df_voltage['cosmos_id'] = br.remap(df_voltage['atlas_id'], source_map='Allen', target_map='Cosmos')
+df_voltage['beryl_id'] = br.remap(df_voltage['atlas_id'], source_map='Allen', target_map='Beryl')
+
+# Remove void and root
+df_voltage = df_voltage.loc[~df_voltage['cosmos_id'].isin(br.acronym2id(['void', 'root']))]
+
+##
+# Add/change features
+# RMS in DB
+for feat in ['rms_ap', 'rms_lf']:
+    df_voltage[feat] = 20 * np.log10(df_voltage[feat])
+
+# Add in spike_count_log
+df_voltage['spike_count_log'] = np.log10(df_voltage['spike_count'] + 1)
+
+# Add in peak_to_trough_ratio + peak_to_trough_duration
+df_voltage = peak_to_trough_ratio(df_voltage)
+df_voltage['peak_to_trough_duration'] = df_voltage['trough_time_secs'] - df_voltage['peak_time_secs']
+
+
+##
+# Regions of DF
+regions = df_voltage[brain_id].unique()
+
+##
+# Run KS test 1-by-1
+
+# Create an array containing two regions to compare
+reg_arr = np.empty(shape=[regions.size, regions.size, 2])  # 10x10x2 for Cosmos
+for i_reg, reg in enumerate(regions):  # Fill in the array
+    reg_arr[i_reg, :, 0] = reg
+    reg_arr[:, i_reg, 1] = reg  # cf TODO Note: this is not used currently but could be useful if re-written
+
+# Fill in result array with Nans
+results_arr = np.empty(shape=[regions.size, regions.size, len(features)])
+results_arr[:] = np.nan
+
+# Run over the half portion of the array
+for i_reg, reg in enumerate(regions):
+    reg_comp_arr = reg_arr[i_reg+1:, i_reg, 0]  # Regions to compare against start from the next available
+    # print(f'-- Region: {reg}, {i_reg}/{len(regions)} ----')
+    # Prepare dataframe per brain regions
+    brain_groupby = df_voltage.groupby(brain_id)
+    df_reg = brain_groupby.get_group(reg)
+
+    for i_reg_comp, reg_comp in enumerate(reg_comp_arr):
+        df_reg_comp = brain_groupby.get_group(reg_comp)
+        # print(f'-- Region compare: {reg_comp}, {i_reg+i_reg_comp+1} ----')
+
+        for i_feat, feat in enumerate(features):
+            ks = ks_2samp(df_reg[feat], df_reg_comp[feat])
+            results_arr[i_reg, i_reg+i_reg_comp+1, i_feat] = ks.pvalue
+
+results_log = np.log10(results_arr)
+
+# TODO Note the code above could be streamlined for readability using indices, but should be as fast:
+'''
+# Get indices over which to do the comparison
+indices = np.triu_indices_from(np.squeeze(reg_arr[:, :, 0]))
+# Need to remove diagonal terms
+diagonal = np.diag_indices(regions.size)
+'''
+
+##
+# Save
+local_result_b = local_result.joinpath(brain_id)
+if not local_result_b.exists():
+    local_result_b.mkdir()
+
+np.save(local_result_b.joinpath('results_log.npy'), results_log, allow_pickle=True)
+np.save(local_result_b.joinpath('results_regions.npy'), regions, allow_pickle=True)
+np.save(local_result_b.joinpath('results_features.npy'), features, allow_pickle=True)
