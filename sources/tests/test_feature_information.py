dd56317823842fb55bc4381bfb5644a766a014c2
Gaelle <gaelle.chapuis.ibl@gmail.com>
2023-12-19 20:45:02 +0100

    cleanup

--- sources/tests/test_feature_information.py
+++ sources/tests/test_feature_information.py
@@ -5,7 +5,7 @@ from ephys_atlas.feature_information import feature_overall_entropy, feature_reg
 
 # Create a dict of count per quantile (N=6) / region (N=3)
 # This mimics the distribution for a given feature
-data = {}
+data = dict()
 data['reg_1'] = np.array([0, 1, 2, 1, 0, 0])
 data['reg_2'] = np.array([0, 0, 2, 1, 1, 0])
 data['reg_3'] = np.array([0, 0, 0, 1, 1, 5])
@@ -15,6 +15,7 @@ counts = counts.transpose()
 nc = np.nansum(counts.values)
 assert nc == 15
 
+
 ##
 def test_feature_overall_entropy(counts):
     info_gain, entropy_feature, entropy_quantiles, entropy_overall = feature_overall_entropy(counts, return_all=True)
@@ -23,18 +24,20 @@ def test_feature_overall_entropy(counts):
     entropy_overall_hand = scipy.stats.entropy(np.array([4, 4, 7]) / nc, base=2)
     np.testing.assert_array_almost_equal(entropy_overall_hand, entropy_overall)
 
+
 def test_feature_region_entropy(counts, nc):
     # Compute with the function
     info_gain_region, entropy_region, entropy_overall = feature_region_entropy(counts, return_all=True)
 
     # Compute entropy for a region "by hand"
-    entropy_hand = list()
+    entropy_region_hand = list()
     for key in data.keys():
-        entropy_hand.append(scipy.stats.entropy(data[key] / nc, base=2))
-    np.testing.assert_array_almost_equal(entropy_hand, entropy_region, decimal=10)
+        entropy_region_hand.append(scipy.stats.entropy(data[key] / nc, base=2))
+    np.testing.assert_array_almost_equal(entropy_region_hand, entropy_region, decimal=10)
 
     entropy_overall_hand = scipy.stats.entropy(np.array([0, 1, 4, 3, 2, 5]) / nc, base=2)
     np.testing.assert_array_almost_equal(entropy_overall_hand, entropy_overall)
+
 
 ##
 # Run the tests
