73c02add38bf22728248a3837889a868b3ea3f87
olivier <olivier.winter@hotmail.fr>
2023-12-22 17:51:06 +0100

    decoding functions to save / load model and cross-validation folds

---
+++ sources/tests/test_decoding.py
@@ -0,0 +1,32 @@
+import unittest
+
+import numpy as np
+from ephys_atlas.decoding import viterbi
+
+
+class TestViterbi(unittest.TestCase):
+
+    def test_viterbi(self):
+        num_hidden_states = 3
+        num_observed_states = 2
+        num_time_steps = 4
+        # Initializes the transition probability matrix (nlatent, nlatent).
+        transition_probs = np.array([
+            [0.1, 0.2, 0.7],
+            [0.1, 0.1, 0.8],
+            [0.5, 0.4, 0.1],
+        ])
+        # Initializes the emission probability matrix. (nlatent, nobs)
+        emission_probs = np.array([
+            [0.1, 0.9],
+            [0.3, 0.7],
+            [0.5, 0.5],
+        ])
+        # Initalizes the initial hidden probabilities (nlatent)
+        init_hidden_probs = np.array([0.1, 0.3, 0.6])
+        # Defines the sequence of observed states (nsteps)
+        observed_states = [1, 1, 0, 1]
+
+        s, p = viterbi(emission_probs, transition_probs, init_hidden_probs, observed_states)
+        assert s == [2, 0, 2, 0]
+        assert p == 0.0212625
