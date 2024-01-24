8a50a516135e7167d8b6b9005efbed3ff093f67c
Gaelle <gaelle.chapuis.ibl@gmail.com>
2024-01-24 14:10:22 +0100

    wf remove info gain

--- sources/elts/02_compute_info_gain_region_pair.py
+++ sources/elts/02_compute_info_gain_region_pair.py
@@ -7,6 +7,7 @@ from iblatlas.atlas import BrainRegions
 from ephys_atlas.data import download_tables, load_voltage_features
 from ephys_atlas.encoding import voltage_features_set
 from one.api import ONE
+from ephys_atlas.entropy import compute_info_gain
 
 ##
 # Download voltage and precise mapping
@@ -17,7 +18,7 @@ br = BrainRegions()
 label = '2023_W51'  # label = '2023_W51_autism'
 mapping = 'Cosmos'
 # local_data_path = Path('/mnt/s0/aggregates/')  # TODO ASK OW
-# save_folder = Path('/mnt/s0/aggregates/')  # TODO ASK OW
+# save_folder = Path('/mnt/s0/ephys-atlas-decoding/entropy/')  # TODO ASK OW
 local_data_path = Path('/Users/gaelle/Documents/Work/EphysAtlas/Data')
 save_folder = Path('/Users/gaelle/Documents/Work/EphysAtlas/Entropy_DF_WF')
 if not save_folder.exists():
@@ -41,7 +42,5 @@ features = voltage_features_set()
 
 ##
 t = time.time()
-joblib.Parallel(n_jobs=5)(joblib.delayed(workflow.info_gain)(df_voltage, feature, mapping, save_folder) for feature in features)
+joblib.Parallel(n_jobs=5)(joblib.delayed(compute_info_gain)(df_voltage, feature, mapping, save_folder) for feature in features)
 print(time.time() - t, len(features), mapping)
-
-
