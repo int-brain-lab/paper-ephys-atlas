753e9ec2b918ed73badefe9613b888722235e008
Gaelle <gaelle.chapuis.ibl@gmail.com>
2024-01-24 12:39:55 +0100

    add pids

--- sources/examples/plots/spatial_derivative.py
+++ sources/examples/plots/spatial_derivative.py
@@ -5,29 +5,26 @@ Plot df voltage
 from pathlib import Path
 import pandas as pd
 from iblatlas.atlas import BrainRegions
-from ephys_atlas.data import load_tables
+from ephys_atlas.data import load_voltage_features
 from ephys_atlas.plots import figure_features_chspace
 from ephys_atlas.spatial_analysis import meta_spatial_derivative
 
 br = BrainRegions()
 label = '2023_W41'
-brain_id = 'cosmos_id'
+mapping = 'Cosmos'
 
 local_data_path = Path('/Users/gaelle/Documents/Work/EphysAtlas/Data')
 local_result_path = Path('/Users/gaelle/Documents/Work/EphysAtlas/Fig3_Result')
-local_fig_path = local_result_path.joinpath(brain_id)
+local_fig_path = local_result_path.joinpath(mapping)
 
-df_voltage, df_clusters, df_channels, df_probes = load_tables(
-    local_data_path.joinpath(label), verify=True)
-
-df_voltage = pd.merge(df_voltage, df_channels, left_index=True, right_index=True).dropna()
-df_voltage['cosmos_id'] = br.remap(df_voltage['atlas_id'], source_map='Allen', target_map='Cosmos')
-df_voltage['beryl_id'] = br.remap(df_voltage['atlas_id'], source_map='Allen', target_map='Beryl')
+df_voltage, df_clusters, df_channels, df_probes = load_voltage_features(
+    local_data_path.joinpath(label), regions=br, mapping=mapping)
 # Do not remove void / root
 ##
 # Prepare the dataframe for a single probe
-pid = '0228bcfd-632e-49bd-acd4-c334cf9213e9'
-##
+# pid = '0228bcfd-632e-49bd-acd4-c334cf9213e9'
+pid = '3d3d5a5e-df26-43ee-80b6-2d72d85668a5'
+
 pid_df = df_voltage[df_voltage.index.get_level_values(0).isin([pid])].copy()
 
 # Create numpy array of xy um (only 2D for plotting)
@@ -45,7 +42,7 @@ for feature in features:
 
 ##
 # Select your features and plot
-fig, axs = figure_features_chspace(pid_df, feat_plt, xy, pid=pid)
+fig, axs = figure_features_chspace(pid_df, feat_plt, xy, pid=pid, mapping=mapping)
 
 ## Note that because we drop nan from the df, channels can vary heavily in distance
 import seaborn
