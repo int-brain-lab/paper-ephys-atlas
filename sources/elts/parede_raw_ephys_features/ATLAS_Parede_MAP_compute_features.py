af8f36e832ba038f53368556655afec5b213f42e
olivier <olivier.winter@hotmail.fr>
2024-01-24 13:08:20 +0000

    add correlograms to sorted features computation

--- sources/elts/parede_raw_ephys_features/ATLAS_Parede_MAP_compute_features.py
+++ sources/elts/parede_raw_ephys_features/ATLAS_Parede_MAP_compute_features.py
@@ -21,7 +21,7 @@ RERUN_ERRORS = False
 
 one = ONE(base_url="https://alyx.internationalbrainlab.org")
 
-if False:
+if True:
     ROOT_PATH = Path("/mnt/s0/ephys-atlas")
     pids, alyx_pids = atlas_pids(one)
 else:
@@ -59,8 +59,6 @@ report = workflow.report(one=one, pids=pids, path_task=ROOT_PATH)
 report.flow.print_report()
 
 ## Runs the sorted features and raw features computations
-
-
 pids_run = report.flow.get_pids_ready('compute_sorted_features', include_errors=RERUN_ERRORS)
 for i, pid in enumerate(pids_run):
     print(i, len(pids_run), pid)
