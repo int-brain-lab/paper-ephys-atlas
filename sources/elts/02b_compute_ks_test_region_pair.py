9758419d796ecd483a3c145c490bee8ebda521e0
Gaelle <gaelle.chapuis.ibl@gmail.com>
2024-01-29 12:46:58 +0100

    ks-test at scale

--- sources/elts/02b_compute_ks_test_region_pair.py
+++ sources/elts/02b_compute_ks_test_region_pair.py
@@ -1,7 +1,7 @@
 '''
-- Compute the entropy by pair of region
+- Compute the ks-test by pair of region
 - Save (flat) dataframe for each feature,
-  containing the 2x region ID and the info gain value in bits
+  containing the 2x region ID and the ks p-value and statistic
 - Aggregate the feature dataframes into 1 for all features
 '''
 import time
@@ -12,7 +12,7 @@ from iblatlas.atlas import BrainRegions
 from ephys_atlas.data import download_tables, load_voltage_features
 from ephys_atlas.encoding import voltage_features_set
 from one.api import ONE
-from ephys_atlas.entropy import compute_info_gain
+from ephys_atlas.entropy import compute_ks_test
 
 # Download voltage and precise mapping
 one = ONE()
@@ -21,10 +21,10 @@ br = BrainRegions()
 label = '2023_W51'
 mapping = 'Beryl'
 local_data_path = Path('/mnt/s0/aggregates/')
-save_folder = Path(f'/mnt/s0/ephys-atlas-decoding/entropy/{label}/')
+save_folder = Path(f'/mnt/s0/ephys-atlas-decoding/kstest/{label}/')
 
 # local_data_path = Path('/Users/gaelle/Documents/Work/EphysAtlas/Data')
-# save_folder = Path(f'/Users/gaelle/Documents/Work/EphysAtlas/Entropy_DF_WF/{label}/')
+# save_folder = Path(f'/Users/gaelle/Documents/Work/EphysAtlas/Entropy_KS/{label}/')
 if not save_folder.parent.exists():
     save_folder.parent.mkdir()
 if not save_folder.exists():
@@ -50,12 +50,12 @@ features = voltage_features_set()
 ##
 # Compute all dataframes in batches
 t = time.time()
-joblib.Parallel(n_jobs=5)(joblib.delayed(compute_info_gain)(df_voltage, feature, mapping, save_folder) for feature in features)
+joblib.Parallel(n_jobs=5)(joblib.delayed(compute_ks_test)(df_voltage, feature, mapping, save_folder) for feature in features)
 print(time.time() - t, len(features), mapping)
 
 ##
 # Aggregate into 1 dataframe
-df_info = dd.read_parquet(list(save_folder.rglob(f'*{mapping}__info_gain.pqt')))
+df_info = dd.read_parquet(list(save_folder.rglob(f'*{mapping}__ks_test.pqt')))
 df_info = df_info.compute()
 # Save
-df_info.to_parquet(save_folder.joinpath(f'{label}__{mapping}__overall_info_gain.pqt'))
+df_info.to_parquet(save_folder.joinpath(f'{label}__{mapping}__overall_ks_test.pqt'))
