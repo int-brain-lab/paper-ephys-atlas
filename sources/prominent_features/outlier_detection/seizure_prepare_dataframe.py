ce601c94871ae2a10a7da0db39ebfd38f2c7300e
GaelleChapuis <gaelle.chapuis.ibl@gmail.com>
2025-02-17 11:38:37 +0100

    prepare seizure dataframe

---
+++ sources/prominent_features/outlier_detection/seizure_prepare_dataframe.py
@@ -0,0 +1,28 @@
+'''
+Prepare the dataset of features for the subject with epileptic seizure
+There are 2 dataframes:
+One dataframe is extracted at a time of normal brain activity,
+the other is extraction at a time of seizure
+'''
+# Get the dataframes from Google drive:
+# https://drive.google.com/drive/u/0/folders/1Qvj16dii3g1myCm40gvoxZ3Ih7tjeBN-
+
+import pandas as pd
+from pathlib import Path
+from brainbox.io.one import SpikeSortingLoader
+from one.api import ONE
+
+one = ONE()
+
+folder_seizure = Path('/Users/gaellechapuis/Documents/Work/EphysAtlas/seizure')
+
+df_baseline=pd.read_parquet(folder_seizure.joinpath('5246af08.pqt'))
+df_seizure=pd.read_parquet(folder_seizure.joinpath('5246af08-seizure.pqt'))
+
+# Add channel information to the dataframes
+
+pid = "5246af08-0730-40f7-83de-29b5d62b9b6d"
+ssl = SpikeSortingLoader(pid=pid, one=one)
+channels = ssl.load_channels()
+
+# TODO once Alyx is back up
