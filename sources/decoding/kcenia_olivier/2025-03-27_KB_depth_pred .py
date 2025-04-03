58bb586ae89885a87178bd1f1dc37d2dc702b090
KceniaB <kcenia.bougrova@research.fchampalimaud.org>
2025-04-03 18:34:41 +0100

    from ipynb to .py

---
+++ sources/decoding/kcenia_olivier/2025-03-27_KB_depth_pred .py
@@ -0,0 +1,8 @@
+#%%
+"""
+    27March2025
+    OW, KB
+    Predicting a continuous variable - cortical depth
+    Merging OW [get data and example model] and MF [atlas acronym / id and depth function] codes
+"""
+# %%
