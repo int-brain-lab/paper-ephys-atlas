beadc5fc11306b0acde458bae43eba061471ad14
KceniaB <kcenia.bougrova@research.fchampalimaud.org>
2025-04-04 11:42:57 +0100

    last code = plot atlas_id along with cortical_depths or other feature (feature not working)

--- sources/decoding/kcenia_olivier/2025-03-27_KB_depth_pred .py
+++ sources/decoding/kcenia_olivier/2025-03-27_KB_depth_pred .py
@@ -24,6 +24,7 @@ import ephys_atlas.decoding
 import ephys_atlas.anatomy
 import ephys_atlas.data
 import ephys_atlas.features
+from iblatlas.atlas import xyz_to_depth
 
 
 import seaborn as sns
@@ -274,12 +275,135 @@ for i, pid in enumerate(pids[:5]):
     brainbox.ephys_plots.plot_probe(data.convert2dict(), ax=ax[i * 3 + 1], show_cbar=False)
     ax[i * 3 + 1].axis('off')
 # %%
+from pathlib import Path
+import matplotlib
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
-# Select only the first 5 pids
-pids = df_voltage.index.get_level_values(0).unique().tolist()[:5]
+import ephys_atlas.data
+import brainbox.ephys_plots
+import iblatlas.atlas
+import iblatlas.plots
+
+def figure_style():
+    sns.set(style="ticks", context="paper",
+            rc={"font.size": 7,
+                "axes.titlesize": 8,
+                "axes.labelsize": 7,
+                "axes.linewidth": 0.5,
+                "axes.spines.top": False,
+                "axes.spines.right": False,
+                "legend.title_fontsize": 7,
+                "lines.linewidth": 1,
+                "lines.markersize": 4,
+                "xtick.labelsize": 6,
+                "ytick.labelsize": 6,
+                "savefig.transparent": False,
+                "xtick.major.size": 2.5,
+                "ytick.major.size": 2.5,
+                "xtick.major.width": 0.5,
+                "ytick.major.width": 0.5,
+                "xtick.minor.size": 2,
+                "ytick.minor.size": 2,
+                "xtick.minor.width": 0.5,
+                "ytick.minor.width": 0.5,
+                })
+    matplotlib.rcParams['pdf.fonttype'] = 42
+    matplotlib.rcParams['ps.fonttype'] = 42
+
+MM_TO_INCH = 1 / 25.4
+
+figure_style()
+FIG_SIZE = (13, 6)
+
+def adjust_figure(fig):
+    # Remove 7.5 mm of whitespace around figure in all directions
+    adjust = 7.5
+    # Depending on the location of axis labels leave a bit more space
+    extra =  5
+    width, height = fig.get_size_inches() / MM_TO_INCH
+    fig.subplots_adjust(top=1-adjust/height, bottom=(adjust + extra)/height,
+                        left=(adjust + extra)/width, right=1-adjust/width)
+
+
+regions = iblatlas.atlas.BrainRegions()
+ba = iblatlas.atlas.AllenAtlas()
+pids = ephys_atlas.data.BENCHMARK_PIDS
 n_pids = len(pids)
 
+# Plot the brain regioons for the set of pids
+fig, ax = plt.subplots(1, 15, figsize=FIG_SIZE, gridspec_kw={'width_ratios': np.r_[np.ones(n_pids), 1.5, 8]})
+ba.plot_top(ax=ax[-1])
+
+
+# # Select only the first 5 pids
+# pids = df_voltage.index.get_level_values(0).unique().tolist()[:5]
+# n_pids = len(pids)
+
+for i, pid in enumerate(pids):
+    df_pid = df_voltage.loc[pid]
+    depth = df_pid.groupby('axial_um').agg(atlas_id=pd.NamedAgg(column='atlas_id', aggfunc='first')).reset_index()
+    brainbox.ephys_plots.plot_brain_regions(depth['atlas_id'].values, channel_depths=depth['axial_um'].values, brain_regions=regions, display=True, ax=ax[i])
+    ax[-1].plot(df_pid['x'].values * 1e6, df_pid['y'].values * 1e6, '.', color='r', markersize=2)
+    ax[-1].plot(df_pid['x'].values[-1] * 1e6, df_pid['y'].values[-1] * 1e6, '*', color='k', markersize=2)
+
+plt.show()
+for i in range(13, 15):
+    ax[i].axis('off')
+adjust_figure(fig)
+
+
+# %% Plot the features for the selected pid
+
+fig, ax = plt.subplots(1, 15, figsize=FIG_SIZE, gridspec_kw={'width_ratios': np.r_[np.ones(n_pids), 1.5, 8]})
+
+feature_name = 'trough_val'
+colormap = 'PuOr_r'
+feature = df_voltage[feature_name].values
+if feature_name in transform:
+    feature = transform[feature_name](feature)
+clim = np.array([np.nanquantile(feature, 0.1), np.nanquantile(feature, 0.9)])
+hlim = np.array([np.nanquantile(feature, 0.01), np.nanquantile(feature, 0.99)])
+
+# np.diff(quants)
+c, x = np.histogram(feature, bins=np.linspace(hlim[0], hlim[1], 64))
+bars = ax[-1].bar(x[:-1], c / np.sum(c), width=np.diff(x)[0])
+cmap = plt.get_cmap(colormap)  # You can choose any colormap you prefer
+norm = plt.Normalize(vmin=clim[0], vmax=clim[1])
+for bar, bin_center in zip(bars, x[:-1]):
+    bar.set_color(cmap(norm(bin_center)))
+
+for i, pid in enumerate(pids):
+    df_pid = df_voltage.loc[pid]
+    feature = df_pid[feature_name].values
+    if feature_name in transform:
+        feature = transform[feature_name](feature)
+    data_bank, x_bank, y_bank = brainbox.plot_base.arrange_channels2banks(
+        data=feature,
+        chn_coords=df_pid[['lateral_um', 'axial_um']].values,
+        pad=True,
+    )
+    data = brainbox.plot_base.ProbePlot(data_bank, x=x_bank, y=y_bank, cmap=colormap)
+    data.clim = clim
+    brainbox.ephys_plots.plot_probe(data.convert2dict(), ax=ax[i], show_cbar=False)
+    if i > 0:
+        # turn off axis labels
+        # ax[i].set_yticklabels([])
+        ax[i].axis('off')
+
+ax[0].set(ylabel='Depth (um)')
+ax[-2].axis('off')
+fig.suptitle(f'{feature_name}')
+plt.show()
+adjust_figure(fig)
+
+# %% double trouble
+fig, ax = plt.subplots(1, n_pids * 3  + 2, figsize=FIG_SIZE,
+                       gridspec_kw={'width_ratios': np.r_[np.ones(n_pids * 3), 1.5, 8]})
+ba.plot_top(ax=ax[-1])
+
+#%% 
 # Set up figure and axes once
 fig, ax = plt.subplots(n_pids, 3, figsize=(1, 4 * n_pids))
 ax = ax.flatten()
@@ -334,3 +458,141 @@ plt.tight_layout()
 plt.show()
 
 # %%
+##############################################
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+import ephys_atlas.data
+import brainbox.ephys_plots
+import iblatlas.atlas
+import iblatlas.plots
+import matplotlib
+
+def figure_style():
+    sns.set(style="ticks", context="paper",
+            rc={"font.size": 7,
+                "axes.titlesize": 8,
+                "axes.labelsize": 7,
+                "axes.linewidth": 0.5,
+                "axes.spines.top": False,
+                "axes.spines.right": False,
+                "legend.title_fontsize": 7,
+                "lines.linewidth": 1,
+                "lines.markersize": 4,
+                "xtick.labelsize": 6,
+                "ytick.labelsize": 6,
+                "savefig.transparent": False,
+                "xtick.major.size": 2.5,
+                "ytick.major.size": 2.5,
+                "xtick.major.width": 0.5,
+                "ytick.major.width": 0.5,
+                "xtick.minor.size": 2,
+                "ytick.minor.size": 2,
+                "xtick.minor.width": 0.5,
+                "ytick.minor.width": 0.5,
+                })
+    matplotlib.rcParams['pdf.fonttype'] = 42
+    matplotlib.rcParams['ps.fonttype'] = 42
+
+MM_TO_INCH = 1 / 25.4
+
+transform = {
+    'polarity': lambda x: np.log10(x + 1 + 1e-6),
+    'rms_lf': lambda x: 20 * np.log10(x),
+    'rms_ap': lambda x: 20 * np.log10(x),
+    'rms_lf_csd': lambda x: 20 * np.log10(x),
+    'spike_count': lambda x: np.log10(x),
+}
+
+figure_style()
+FIG_SIZE = (13, 6)
+
+
+def adjust_figure(fig):
+    # Remove 7.5 mm of whitespace around figure in all directions
+    adjust = 7.5
+    # Depending on the location of axis labels leave a bit more space
+    extra =  5
+    width, height = fig.get_size_inches() / MM_TO_INCH
+    fig.subplots_adjust(top=1-adjust/height, bottom=(adjust + extra)/height,
+                        left=(adjust + extra)/width, right=1-adjust/width)
+
+regions = iblatlas.atlas.BrainRegions()
+ba = iblatlas.atlas.AllenAtlas()
+pids = df_voltage.index.get_level_values(0).unique().tolist()[:3]
+df_voltage_3 = df_voltage.loc[pids]
+df_voltage = df_voltage_3
+n_pids = len(pids)
+
+
+# %% Plot the brain regioons for the set of pids
+widths = np.r_[np.ones(n_pids), 1.5, 8]
+fig, ax = plt.subplots(1, len(widths), figsize=FIG_SIZE, gridspec_kw={'width_ratios': widths})
+ba.plot_top(ax=ax[-1])
+
+for i, pid in enumerate(pids):
+    df_pid = df_voltage.loc[pid]
+    depth = df_pid.groupby('axial_um').agg(atlas_id=pd.NamedAgg(column='atlas_id', aggfunc='first')).reset_index()
+    brainbox.ephys_plots.plot_brain_regions(depth['atlas_id'].values, channel_depths=depth['axial_um'].values, brain_regions=regions, display=True, ax=ax[i])
+    ax[-1].plot(df_pid['x'].values * 1e6, df_pid['y'].values * 1e6, '.', color='r', markersize=2)
+    ax[-1].plot(df_pid['x'].values[-1] * 1e6, df_pid['y'].values[-1] * 1e6, '*', color='k', markersize=2)
+
+plt.show()
+for i in range(len(ax) - 2, len(ax)):
+    ax[i].axis('off') #changed
+
+adjust_figure(fig)
+
+# %% Plot the features for the selected pid
+
+widths = np.r_[np.ones(n_pids), 1.5, 8]
+fig, ax = plt.subplots(1, len(widths), figsize=FIG_SIZE, gridspec_kw={'width_ratios': widths})
+
+feature_name = 'trough_val'
+colormap = 'PuOr_r'
+feature = df_voltage[feature_name].values
+if feature_name in transform:
+    feature = transform[feature_name](feature)
+clim = np.array([np.nanquantile(feature, 0.1), np.nanquantile(feature, 0.9)])
+hlim = np.array([np.nanquantile(feature, 0.01), np.nanquantile(feature, 0.99)])
+
+# np.diff(quants)
+c, x = np.histogram(feature, bins=np.linspace(hlim[0], hlim[1], 64))
+bars = ax[-1].bar(x[:-1], c / np.sum(c), width=np.diff(x)[0])
+cmap = plt.get_cmap(colormap)  # You can choose any colormap you prefer
+norm = plt.Normalize(vmin=clim[0], vmax=clim[1])
+for bar, bin_center in zip(bars, x[:-1]):
+    bar.set_color(cmap(norm(bin_center)))
+
+for i, pid in enumerate(pids):
+    df_pid = df_voltage.loc[pid]
+    feature = df_pid[feature_name].values
+    if feature_name in transform:
+        feature = transform[feature_name](feature)
+    data_bank, x_bank, y_bank = brainbox.plot_base.arrange_channels2banks(
+        data=feature,
+        chn_coords=df_pid[['lateral_um', 'axial_um']].values,
+        pad=True,
+    )
+    data = brainbox.plot_base.ProbePlot(data_bank, x=x_bank, y=y_bank, cmap=colormap)
+    data.clim = clim
+    brainbox.ephys_plots.plot_probe(data.convert2dict(), ax=ax[i], show_cbar=False)
+    if i > 0:
+        # turn off axis labels
+        # ax[i].set_yticklabels([])
+        ax[i].axis('off')
+
+ax[0].set(ylabel='Depth (um)')
+ax[-2].axis('off')
+fig.suptitle(f'{feature_name}')
+plt.show()
+adjust_figure(fig)
+
+# # %% double trouble
+# fig, ax = plt.subplots(1, n_pids * 3  + 2, figsize=FIG_SIZE,
+#                        gridspec_kw={'width_ratios': np.r_[np.ones(n_pids * 3), 1.5, 8]})
+# ba.plot_top(ax=ax[-1])
