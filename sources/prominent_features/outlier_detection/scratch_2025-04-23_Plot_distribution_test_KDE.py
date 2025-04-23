23289375bbf4070332c2f65466a646aa4fbd06b4
GaelleChapuis <gaelle.chapuis.ibl@gmail.com>
2025-04-23 23:09:46 +0200

    plot script

--- sources/prominent_features/outlier_detection/scratch_2025-04-23_Plot_distribution_test_KDE.py
+++ sources/prominent_features/outlier_detection/scratch_2025-04-23_Plot_distribution_test_KDE.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 from ephys_atlas.data import load_voltage_features, download_tables
 from ephys_atlas.features import voltage_features_set
 from pathlib import Path
-import pandas as pd
-import tqdm
+import seaborn
 from one.api import ONE
 ##
 import numpy as np
@@ -54,13 +53,7 @@ df_voltage, _, _, _ = \
 # -- Check that features are in df columns
 features = sorted(list(set(df_voltage.columns).intersection(set(features))))
 
-
-features = ['alpha_mean',
-            'psd_beta',
-            'psd_delta',
-            'psd_gamma',
-            'rms_ap',
-            'rms_lf']
+features = ['rms_ap']
 
 ##
 # Take PID into new DF and drop PID from df_voltage (baseline distribution)
@@ -69,68 +62,40 @@ idx = df_voltage[df_voltage['pids'] == pid].index
 
 df_new = df_voltage.loc[idx].copy()
 df_base = df_voltage.drop(idx).copy()
-##
-# -- Plot example
-feature = 'alpha_mean'
-fig, ax = plt.subplots()
-series = select_series(df_voltage, feature, acronym='void')
-plot_histogram(series, ax=ax, xlabel=feature, title=None, bins=np.linspace(0,2000,100))
-plt.show()
-
-y = df_new[feature].values
-plt.scatter(y, 2000 * np.ones(y.shape))
 
 
 ##
 # Regions
 regions = np.unique(df_new[mapping + '_id']).astype(int)
+region = regions[0]
 
-for count, region in tqdm.tqdm(enumerate(regions), total=len(regions)):
+# Load data for that regions
+df_region = select_series(df_voltage, features=features,
+                          acronym=None, id=region, mapping=mapping)
 
-    # Load data for that regions
-    df_region = select_series(df_voltage, features=features,
-                              acronym=None, id=region, mapping=mapping)
+# Get channel indices that are in region, keeping only feature values
+df_new_compute = select_series(df_new, features=features,
+                               acronym=None, id=region, mapping=mapping)
+# # Get channel indices that are in region, but keeping all info besides features
+# idx_reg = np.where(df_new[mapping + '_id'] == region)
+# df_new_compute2 = df_new.iloc[idx_reg].copy()
 
-    # Get channel indices that are in region, keeping only feature values
-    df_new_compute = select_series(df_new, features=features,
-                                   acronym=None, id=region, mapping=mapping)
-    # Get channel indices that are in region, but keeping all info besides features
-    idx_reg = np.where(df_new[mapping + '_id'] == region)
-    df_new_compute2 = df_new.iloc[idx_reg].copy()
+# Compute test/train sets
+train_data = df_region.to_numpy()
+test_data = df_new_compute.to_numpy()
 
-
-    train_data = df_region.to_numpy()
-    test_data = df_new_compute.to_numpy()
-
-    # For all channels at once, test if outside the distribution for the given features
-    score_out = detect_outliers_kde(train_data, test_data)
-
-    # Save into new column
-    df_new_compute2[feature + '_q'] = score_out  # TODO check ordering of channels
-
-
-    # Concatenate dataframes
-    if count == 0:
-        df_save = df_new_compute2.copy()
-    else:
-        df_save = pd.concat([df_save, df_new_compute2])
-
+# score_out = detect_outliers_kde(train_data, test_data)
+score_out = detect_outliers_kde(train_data, test_data)
+df_new_compute['score'] = score_out
 ##
-# Assign high and low values for picked threshold
-p_thresh = 0.01
-for feature in features:
-    df_save[feature + '_extremes'] = 0
-    df_save.loc[df_save[feature + '_q'] < p_thresh, feature + '_extremes'] = 1
-
-##
-# Plot
-from ephys_atlas.plots import figure_features_chspace
-pid_ch_df = df_save.copy()
-# Create numpy array of xy um (only 2D for plotting)
-xy = pid_ch_df[['lateral_um', 'axial_um']].to_numpy()
-# Plot
-fig, axs = figure_features_chspace(pid_ch_df, features, xy, pid=pid, mapping=mapping, plot_rect=plot_probe_rect2)
-fig, axs = figure_features_chspace(pid_ch_df, [s + '_q' for s in features], xy, pid=pid, mapping=mapping, plot_rect=plot_probe_rect2)
-fig, axs = figure_features_chspace(pid_ch_df, [s + '_extremes' for s in features], xy, pid=pid, mapping=mapping, plot_rect=plot_probe_rect2)
-
+# -- Plot example
+feature = features[0]
+fig, ax = plt.subplots()
+series = select_series(df_voltage, feature, acronym='void')
+plot_histogram(series, ax=ax, xlabel=feature, title=None, bins=np.linspace(0,2000,100))
 plt.show()
+
+y = df_new_compute[feature].values
+plt.scatter(y, 2000 * np.ones(y.shape))
+
+seaborn.scatterplot(data=df_new_compute, x=feature, y=2000, hue='score')
